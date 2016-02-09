c7b8c0fdbbf209ae8bfdfc222b3f523b709aa93b
xstrtok <sweepy1982@gmail.com>
2016-02-09 16:36:39 +0200

    [BEN-110] - List database seeds in DatabaseSeeder. Commented for now, except for UserSeeder.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,12 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         $this->call(UsersTableSeeder::class);
+        // $this->call(UsersRolesSeeder::class);
+        // $this->call(UsersSubrolesSeeder::class);
+        // $this->call(GendersTableSeeder::class);
+        // $this->call(MaritalStatusSeeder::class);
+        // $this->call(EducationTableSeeder::class);
+        // $this->call(LegalStatusSeeder::class);
 
         Model::reguard();
     }
