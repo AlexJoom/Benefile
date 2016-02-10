b666e99aee78f4bafab38f753fe4e296d08f1754
spyninja <nikolakispyros@gmail.com>
2016-02-10 10:50:45 +0200

    BEN-73 js is capable of renaming the language selection dropdowns

--- public/js/records/basic_info.js
+++ public/js/records/basic_info.js
@@ -16,18 +16,24 @@ $(document).ready(function(){
     // add more languages
     $("a.color-green").on("click", function(){
         var $copy = $(".language-div").clone();
+        // change the class so they won't be cloned every time all of them
         $copy.removeClass("language-div").addClass("added-div");
+        // make the add button invisible and the remove button visible
         $copy.find(".color-green").hide();
         $copy.find(".color-red").show();
+        // set new name to dropdowns so that the controller can view them all
+        $langs_count++;
+        $copy.find(".language-selection").attr("name", $copy.find(".language-selection").attr("name") + $langs_count);
+        $copy.find(".level-selection").attr("name", $copy.find(".level-selection").attr("name") + $langs_count);
+        // append cloned element to parent
         $copy.appendTo($parent);
-        //$langs_count++;
     });
 
+    // remove element after remove button is clicked
     $("body").on("click", ".color-red", function(){
         $(this).parents(".added-div").hide();
-        //$langs_count--;
     });
 });
 
-//var $langs_count = 1;
+var $langs_count = 0;
 var $parent = $(".language-div").parent();
