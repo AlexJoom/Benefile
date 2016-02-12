023e97f98772b6e8d3a8ae61dc2d52c5051c83c0
spyninja <nikolakispyros@gmail.com>
2016-02-12 10:46:53 +0200

    added app/config/app.php

--- config/app.php
+++ config/app.php
@@ -148,7 +148,7 @@ return [
         /*
          * Include Laravel Forms
          */
-//        Collective\Html\HtmlServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -198,8 +198,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-//        'Form'      => Collective\Html\FormFacade::class,
-//        'Html'      => Collective\Html\HtmlFacade::class,
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
 
     ],
 
