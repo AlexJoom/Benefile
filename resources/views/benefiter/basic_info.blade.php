e020fe68308f88fb6e054d9c9829dfc10b0a0290
spyninja <nikolakispyros@gmail.com>
2016-02-12 17:04:13 +0200

    merged panel with view in basic_info.blade.php and deleted the stand alone panel for records

--- resources/views/benefiter/basic_info.blade.php
+++ resources/views/benefiter/basic_info.blade.php
@@ -1,13 +1,64 @@
-@extends('.new_record_panel')
+@extends('layouts.mainPanel')
 
-@section('css')
+@section('panel-title')
+    Νέος Ωφελούμενος
+@stop
+
+@section('panel-headLinks')
+    <link href="{{asset('css/records/new_record_panel.css')}}" rel="stylesheet" type="text/css">
     <link href="{{asset('css/records/basic_info.css')}}" rel="stylesheet" type="text/css">
 @stop
 
-@section('view-content')
+@section('main-window-content')
+
+    {{-- actions refering to records --}}
+
+    <?php
+        /* check which tab is selected */
+        $basic_selected = '';
+        $medical_selected = '';
+        $legal_selected = '';
+        $social_selected = '';
+        if(isset($tab)){
+            if($tab === "medical"){
+                $medical_selected = 'selected';
+            } else if($tab === "legal"){
+                $legal_selected = 'selected';
+            } else if($tab === "social"){
+                $social_selected = 'selected';
+            }
+        } else {
+            $basic_selected = 'selected';
+        }
+    ?>
+
+    <div class="no-margin light-green-background width-100-percent" id="actions">
+    {{--<div style="width: 100%; background-color: red;">--}}
+        <div class="row width-100-percent">
+            <div class="col-md-3 record-panel-title">
+                <a class="white {{ $basic_selected }}" href="{{ url('/new-benefiter/basic-info') }}">ΒΑΣΙΚΑ ΣΤΟΙΧΕΙΑ</a>
+            </div>
+
+            <div class="col-md-3 record-panel-title">
+                <a class="white {{ $medical_selected }}" href="">ΙΑΤΡΙΚΟΣ ΦΑΚΕΛΟΣ</a>
+            </div>
+
+            <div class="col-md-3 record-panel-title">
+                <a class="white {{ $legal_selected }}" href="">ΝΟΜΙΚΟΣ ΦΑΚΕΛΟΣ</a>
+            </div>
+
+            <div class="col-md-3 record-panel-title">
+                <a class="white {{ $social_selected }}" href="">ΚΟΙΝΩΝΙΚΟΣ ΦΑΚΕΛΟΣ</a>
+            </div>
+        </div>
+        {{-- The abone three options will be removed in order to be added dynamically from another view. --}}
+
+    </div>
+
     @include('partials.forms.basic_info_form')
 @stop
 
-@section('js')
+@section('panel-scripts')
+    <script src="{{ asset('js/records/selectNewRecordInMainPanel.js') }}"></script>
     <script src="{{asset('js/records/basic_info.js')}}"></script>
 @stop
