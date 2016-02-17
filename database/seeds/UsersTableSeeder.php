652d44c3073e745c3d8e61f6bdd17b18a4339b19
xstrtok <sweepy1982@gmail.com>
2016-02-17 18:15:49 +0200

    [BEN-110] - Middle table for chronic conditions. Rollback is broken ATM. Needs fix.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,16 +14,16 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         // ADMIN USER
-//        User::create(array(
-//            'name' => 'Christos',
-//            'lastname' => 'Dimizas',
-//            'email' => 'christos.dimizas@gmail.com',
-//            'password' => Hash::make('1234qwer'),
-//            'user_role_id' => 1,
-//            'user_subrole_id' => 0,
-//            'activation_status' => 1,
-//            'is_deactivated' => 0,
-//        ));
+       User::create(array(
+           'name' => 'Christos',
+           'lastname' => 'Dimizas',
+           'email' => 'christos.dimizas@gmail.com',
+           'password' => Hash::make('1234qwer'),
+           'user_role_id' => 1,
+           'user_subrole_id' => 0,
+           'activation_status' => 1,
+           'is_deactivated' => 0,
+       ));
 //
 //        // TEST USERS (WAIT TO BE ACTIVE)
 //        User::create(array(
