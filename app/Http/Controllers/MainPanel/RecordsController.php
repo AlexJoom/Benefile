fffd76cd6527aff5834d8901ac4b8ae9f98c9a87
spyninja <nikolakispyros@gmail.com>
2016-02-17 11:30:30 +0200

    BEN-73 display validation errors in basic info view

--- app/Http/Controllers/MainPanel/RecordsController.php
+++ app/Http/Controllers/MainPanel/RecordsController.php
@@ -14,26 +14,29 @@ class RecordsController extends Controller
 {
     private $basicInfoService;
     private $socialFolderService;
+    private $languages;
+    private $languageLevels;
 
     public function __construct(){
         // initialize basic info service
         $this->basicInfoService = new BasicInfoService();
         // initialize social folder service
         $this->socialFolderService = new SocialFolderService();
+        // set $languages and $languagesLevels
+        $this->languages = $this->basicInfoService->getAllLanguages();
+        $this->languageLevels = $this->basicInfoService->getAllLanguageLevels();
     }
 
     // get basic info view
     public function getBasicInfo(){
-        $languages = $this->basicInfoService->getAllLanguages();
-        $languageLevels = $this->basicInfoService->getAllLanguageLevels();
-        return view('benefiter.basic_info')->with("languages", $languages)->with("languageLevels", $languageLevels);
+        return view('benefiter.basic_info')->with("languages", $this->languages)->with("languageLevels", $this->languageLevels);
     }
 
     // post from basic info form
     public function postBasicInfo(Request $request){
         $validator = $this->basicInfoService->basicInfoValidation($request->all());
         if($validator->fails()){
-            return view('benefiter.basic_info')->withErrors($validator->errors()->all());
+            return view('benefiter.basic_info')->with("languages", $this->languages)->with("languageLevels", $this->languageLevels)->withErrors($validator->errors()->all());
         } else {
             $this->basicInfoService->saveBasicInfoToDB($request->all());
             return 'success';
--- resources/views/benefiter/basic_info.blade.php
+++ resources/views/benefiter/basic_info.blade.php
@@ -7,12 +7,23 @@
 @section('panel-headLinks')
     <link href="{{ asset('/plugins/datepicker/css/datepicker.css') }}" rel="stylesheet" type="text/css">
     <link href="{{asset('css/records/new_record_panel.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/records/validation_errors.css') }}" rel="stylesheet" type="text/css">
     <link href="{{asset('css/records/record_form.css')}}" rel="stylesheet" type="text/css">
 @stop
 
 @section('main-window-content')
 
     @include('partials.select-panel')
+
+    @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
     @include('partials.forms.basic_info_form')
 @stop
