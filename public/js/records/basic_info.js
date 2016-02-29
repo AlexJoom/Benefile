8de9991c24612538ac62e7cbd051af42e1dbc13e
spyninja <nikolakispyros@gmail.com>
2016-02-29 15:31:08 +0200

    added in basic_info.js event handlers to change label's color when a textarea is selected

--- public/js/records/basic_info.js
+++ public/js/records/basic_info.js
@@ -57,6 +57,18 @@ $(document).ready(function(){
         $(this).parents(".form-group").first().find("label").removeClass("focused");
     });
 
+    // when input of type textarea is focused, change the color of the label
+    $("body").on("focus", "textarea", function(){
+        var $labelParent = $(this).parents(".form-group").first();
+        var $label = $labelParent.find("label").first();
+        $label.addClass("focused")
+    });
+
+    // when input is blurred change to normal color
+    $("body").on("blur", "textarea", function(){
+        $(this).parents(".form-group").first().find("label").removeClass("focused");
+    });
+
     // add more referrals in basic info
     $("body").on("click", ".add-ref", function(){
         var $copy = $(".basic_info_referral").clone();
