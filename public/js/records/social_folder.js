ff410770f02daa1d915b3f7a84bd7bfa970d23ed
spyninja <nikolakispyros@gmail.com>
2016-03-01 16:32:30 +0200

    created a separate js file for forms and cleared {basic_info/social_folder}.js

--- public/js/records/social_folder.js
+++ public/js/records/social_folder.js
@@ -1,30 +1,4 @@
 $(document).ready(function(){
-    // when input of type text is focused, change the color of the label
-    $("body").on("focus", "input", function(){
-        if($(this).attr("type") == "text") {
-            var $labelParent = $(this).parents(".form-group").first();
-            var $label = $labelParent.find("label").first();
-            $label.addClass("focused");
-        }
-    });
-
-    // when input is blurred change to normal color
-    $("body").on("blur", "input", function(){
-        $(this).parents(".form-group").first().find("label").removeClass("focused");
-    });
-
-    // when input of type textarea is focused, change the color of the label
-    $("body").on("focus", "textarea", function(){
-        var $labelParent = $(this).parents(".form-group").first();
-        var $label = $labelParent.find("label").first();
-        $label.addClass("focused")
-    });
-
-    // when input is blurred change to normal color
-    $("body").on("blur", "textarea", function(){
-        $(this).parents(".form-group").first().find("label").removeClass("focused");
-    });
-
     // slide toggle to add a new session
     $(".new-session").hide();
     $("#add-new-session").on("click", function(){
