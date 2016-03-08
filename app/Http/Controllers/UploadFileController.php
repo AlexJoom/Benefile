10ab2afe659e7ed53e1ae074730944cbdbd9650c
spyninja <nikolakispyros@gmail.com>
2016-03-08 17:34:59 +0200

    BEN-241 dates are now stored correctly, language_interpreter_needed and is_benefiter_working fixed too

--- app/Http/Controllers/UploadFileController.php
+++ app/Http/Controllers/UploadFileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Benefiters_Tables_Models\Benefiter;
 use App\Models\Benefiters_Tables_Models\File_import_schema;
+use App\Services\DatesHelper;
 use Illuminate\Support\Facades\Input;
 use Carbon\Carbon;
 
@@ -73,17 +74,20 @@ class UploadFileController extends Controller
 
     // selects and returns all the columns - values inserted from file that correspond to the benefiters DB table
     private function selectBenefitersColumnsAndValuesFromFileRow($singleRow){
+        $datesHelper = new DatesHelper();
         $singleRow->gender = $this->getGenderId($singleRow->gender);
         $singleRow->marital_status = $this->getMaritalStatusId($singleRow->marital_status);
         $singleRow->education = $this->getEducationId($singleRow->education);
+        $singleRow->language_interpreter_needed = $this->getYesOrNoId($singleRow->language_interpreter_needed);
+        $singleRow->is_benefiter_working = $this->getYesOrNoId($singleRow->is_benefiter_working);
         return array(
                 'folder_number' => $singleRow->folder_number,
                 'name' => $singleRow->name,
                 'lastname' => $singleRow->lastname,
                 'fathers_name' => $singleRow->fathers_name,
                 'mothers_name' => $singleRow->mothers_name,
-                'birth_date' => $singleRow->birth_date,
-                'arrival_date' => $singleRow->arrival_date,
+                'birth_date' => $datesHelper->makeDBFriendlyDate($singleRow->birth_date),
+                'arrival_date' => $datesHelper->makeDBFriendlyDate($singleRow->arrival_date),
                 'gender_id' => $singleRow->gender,
                 'origin_country' => $singleRow->origin_country,
                 'nationality_country' => $singleRow->nationality_country,
@@ -107,10 +111,12 @@ class UploadFileController extends Controller
 
     // get id from gender name
     private function getGenderId($genderFromFile){
+        $genderFromFile = iconv('utf-8', 'ascii//TRANSLIT', $genderFromFile);
         $genders = \DB::table('genders_lookup')->get();
         // change the gender name to gender id
         foreach($genders as $gender){
-            if($genderFromFile == $gender->gender){
+            $gender->gender = iconv('utf-8', 'ascii//TRANSLIT', $gender->gender);
+            if(strcasecmp($genderFromFile, $gender->gender) == 0){
                 return $gender->id;
             }
         }
@@ -120,10 +126,12 @@ class UploadFileController extends Controller
 
     // get id from marital status name
     private function getMaritalStatusId($maritalStatusFromFile){
+        $maritalStatusFromFile = iconv('utf-8', 'ascii//TRANSLIT', $maritalStatusFromFile);
         $marital_statuses = \DB::table('marital_status_lookup')->get();
         // change the marital status name to the marital status id
         foreach($marital_statuses as $marital_status){
-            if($maritalStatusFromFile == $marital_status->marital_status_title){
+            $marital_status->marital_status_title = iconv('utf-8', 'ascii//TRANSLIT', $marital_status->marital_status_title);
+            if(strcasecmp($maritalStatusFromFile, $marital_status->marital_status_title) == 0){
                 return $marital_status->id;
             }
         }
@@ -133,14 +141,39 @@ class UploadFileController extends Controller
 
     // get id from education name
     private function getEducationId($educationFromFile){
+        $educationFromFile = iconv('utf-8', 'ascii//TRANSLIT', $educationFromFile);
         $educations = \DB::table('education_lookup')->get();
         // change the education name to the education name id
         foreach($educations as $education){
-            if($educationFromFile == $education->education_title){
+            $education->education_title = iconv('utf-8', 'ascii//TRANSLIT', $education->education_title);
+            if(strcasecmp($educationFromFile, $education->education_title) == 0){
                 return $education->id;
             }
         }
         // education not found
         return null;
+    }
+
+    // get id for yes or no answer
+    private function getYesOrNoId($answer){
+//        $answer = iconv('utf-8', 'ascii//TRANSLIT', utf8_encode($answer));
+//        $yes = iconv('utf-8', 'ascii//TRANSLIT', utf8_encode('ναι'));
+//        $answer2= iconv('ascii','utf-8',  $answer);
+//        $yes2= iconv('ascii','utf-8',  'ναι');
+//        if((strcasecmp($answer, $yes)) == 0){
+//            return 1;
+//        } else {
+//            return 0;
+//        }
+        $yes = array(
+            0 => 'ΝΑΙ',
+            1 => 'ναι',
+            2 => 'Ναι',
+        );
+        if(in_array($answer, $yes)){
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
