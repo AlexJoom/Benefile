282a864130141b3b7ad0931ed24b45217ba1d3b6
spyninja <nikolakispyros@gmail.com>
2016-03-08 17:33:36 +0200

    changed marital status seeder to work with file db inserts

--- database/seeds/MaritalStatusSeeder.php
+++ database/seeds/MaritalStatusSeeder.php
@@ -15,8 +15,8 @@ class MaritalStatusSeeder extends Seeder
             array(
                 array('marital_status_title' => "Άγαμος"),
                 array('marital_status_title' => "Έγγαμος"),
-                array('marital_status_title' => "Διαζευγμένος/η"),
-                array('marital_status_title' => "Χήρος/α"),
+                array('marital_status_title' => "Διαζευγμένος"),
+                array('marital_status_title' => "Χήρος"),
                 array('marital_status_title' => "Εν διαστάση")
             )
         );
