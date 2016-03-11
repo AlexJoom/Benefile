2fcb67f166fc05d13de94aff5bf04292587c34e9
spyninja <nikolakispyros@gmail.com>
2016-03-11 11:04:42 +0200

    BEN-258 added error handling msgs for file upload

---
+++ resources/lang/gr/upload_file_errors.php
@@ -0,0 +1,12 @@
+<?php
+
+    return array(
+        'import_csv_row_error1' => 'Η εγγραφή με αριθμό φακέλου ',
+        'import_csv_row_error2' => ' δεν αποθηκεύτηκε.',
+        'insert_benefiter_error' => 'Δεν αποθηκεύτηκε ο ωφελούμενος με αριθμό φακέλου: ',
+        'language_not_found_error' => 'Δε βρέθηκε κι επομένως δεν αποθηκεύτηκε η γλώσσα: ',
+        'level_not_found_error' => 'Δε βρέθηκε κι επομένως δεν αποθηκεύτηκε το επίπεδο γλώσσας: ',
+        'insert_legal_status_error' => 'Δεν αποθηκεύτηκε το νομικό καθεστώς με τίτλο: ',
+        'benefiter_with_id' => ' για τον ωφελούμενο: ',
+        'legal_status_not_found_error' => 'Δε βρέθηκε το νομικό καθεστώς με τίτλο: ',
+    );
