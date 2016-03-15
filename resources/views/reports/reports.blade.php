742de9602998a90d9a47efd323b0e70316cafca2
spyninja <nikolakispyros@gmail.com>
2016-03-15 17:21:20 +0200

    BEN-270 view created to display the users roles counters

--- resources/views/reports/reports.blade.php
+++ resources/views/reports/reports.blade.php
@@ -1,9 +1,54 @@
+<?php
+    $p = 'reports.';
+?>
+
 @extends('layouts.mainPanel')
 
 @section('panel-title')
     @lang('layouts/mainPanel.reports')
 @stop
 
-@section('main-window-content')
+@section('panel-headLinks')
+    <link href="{{ asset('css/records/record_form.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/reports/reports.css') }}" rel="stylesheet" type="text/css">
+@stop
 
+@section('main-window-content')
+    <div class="users-report form-section">{{-- no-bottom-border"> --}}
+        <div class="underline-header">
+            <h1 class="record-section-header padding-left-right-15">@lang($p."users")</h1>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div id="doctors" class="col-md-3">
+                    <img class="make-inline width-85px" src="{{ asset('/img/benefile_3_doctors.jpg') }}" />
+                    <div class="make-inline">
+                        <p class="users-title">ΙΑΤΡΟΙ</p>
+                        <p class="users-counter">{{ $users_roles_count['doctors'] }}</p>
+                    </div>
+                </div>
+                <div id="legals" class="col-md-3">
+                    <img class="make-inline width-85px" src="{{ asset('/img/benefile_3_legals.jpg') }}" />
+                    <div class="make-inline">
+                        <p class="users-title">ΔΙΚΗΓΟΡΟΙ</p>
+                        <p class="users-counter">{{ $users_roles_count['legals'] }}</p>
+                    </div>
+                </div>
+                <div id="psychologists" class="col-md-3">
+                    <img class="make-inline width-85px" src="{{ asset('/img/benefile_3_psychologists.jpg') }}" />
+                    <div class="make-inline">
+                        <p class="users-title">ΨΥΧΟΛΟΓΟΙ</p>
+                        <p class="users-counter">{{ $users_roles_count['psychologists'] }}</p>
+                    </div>
+                </div>
+                <div id="socials" class="col-md-3">
+                    <img class="make-inline width-85px" src="{{ asset('/img/benefile_3_socials.jpg') }}" />
+                    <div class="make-inline">
+                        <p class="users-title">ΚΟΙΝ. ΛΕΙΤΟΥΡΓΟΙ</p>
+                        <p class="users-counter">{{ $users_roles_count['socials'] }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
