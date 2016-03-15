5ad5ca8ca5cf0b125418bd1a99b9089446340b69
spyninja <nikolakispyros@gmail.com>
2016-03-15 15:36:02 +0200

    created a reports service and initialized it through reports controller

--- app/Http/Controllers/MainPanel/ReportsController.php
+++ app/Http/Controllers/MainPanel/ReportsController.php
@@ -6,15 +6,21 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Services\ReportsService;
 
 class ReportsController extends Controller
 {
+    private $reportsService;
+
     public function __construct(){
         // only for logged in users
         $this->middleware('auth');
+        // initialize reports service
+        $this->reportsService = new ReportsService();
     }
 
     public function getReports(){
+        // do stuff with reports service here
         return View('reports.reports');
     }
 }
