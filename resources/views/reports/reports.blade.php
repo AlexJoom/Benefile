5558b58b87a0020b2ed07ba03f8bedd90c24ff2b
spyninja <nikolakispyros@gmail.com>
2016-03-16 14:47:05 +0200

    BEN-274 if nothing is returned from the controller display an empty chart

--- resources/views/reports/reports.blade.php
+++ resources/views/reports/reports.blade.php
@@ -141,6 +141,18 @@
                         data: [ @foreach($benefiters_work_title as $key => $value) {!! $value !!}, @endforeach ]
                     }
                 ]
+                @else
+                labels: [ "" ],
+                datasets: [
+                    {
+                        label: "-",
+                        fillColor: "rgba(151,187,205,0.5)",
+                        strokeColor: "rgba(151,187,205,0.8)",
+                        highlightFill: "rgba(151,187,205,0.75)",
+                        highlightStroke: "rgba(151,187,205,1)",
+                        data: [ 0 ]
+                    }
+                ]
                 @endif
             };
             new Chart($benefiters_work_title).Bar($data, {});
