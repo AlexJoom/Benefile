bbeab1ae8a881a2e1bb4b0c0698fac4649254d4e
spyninja <nikolakispyros@gmail.com>
2016-03-21 12:09:19 +0200

    BEN-292 get legal status with id transformed to name and made some benefiter id specific queries [not sure if we gonna use it now but uploaded for future reference (maybe?)]

--- app/Services/DownloadFileService.php
+++ app/Services/DownloadFileService.php
@@ -1,25 +1,47 @@
 <?php namespace App\Services;
 
+use App\Models\Benefiters_Tables_Models\Benefiter;
 use App\Services\ReportsService;
+use App\Services\BasicInfoService;
+use Illuminate\Support\Facades\Log;
 
 class DownloadFileService{
 
     private $reportsService;
+    private $basicInfoService;
+    private $allGenders;
+    private $allMaritalStatuses;
+    private $allEducationTitles;
+    private $allWorkTitles;
+    private $allLegalStatuses;
+    private $allLanguages;
+    private $allLanguageLevels;
+    private $allReferrals;
 
     public function __construct(){
-        // initialize reports service
         $this->reportsService = new ReportsService();
+        $this->basicInfoService = new BasicInfoService();
+        // make arrays with DB queries from functions inside the above or the present services
+        $this->allGenders = $this->getAllGenders()->toArray();
+        $this->allMaritalStatuses = $this->reportsService->getAllMaritalStatuses()->toArray();
+        $this->allEducationTitles = $this->reportsService->getAllEducationTitles()->toArray();
+        $this->allWorkTitles = $this->reportsService->getAllWorkTitles()->toArray();
+        $this->allLegalStatuses = $this->reportsService->getAllLegalStatuses()->toArray();
+        $this->allLanguages = $this->basicInfoService->getAllLanguages()->toArray();
+        $this->allLanguageLevels = $this->basicInfoService->getAllLanguageLevels()->toArray();
+        $this->allReferrals = $this->getAllReferrals()->toArray();
     }
 
     // make csv file
     public function makeCSVFileUsingBenefitersIds($idsString){
         $benefitersIds = $this->getBenefitersIdsArrayFromString($idsString);
-        $allGenders = $this->getAllGenders();
-        $allMaritalStatuses = $this->reportsService->getAllMaritalStatuses();
-        $allEducationTitles = $this->reportsService->getAllEducationTitles();
-        $allWorkTitles = $this->reportsService->getAllWorkTitles();
         foreach($benefitersIds as $benefiterId){
             // make benefiter_id specific DB queries
+            $benefiter = Benefiter::find($benefiterId);
+            $benefiterLegalStatuses = \DB::table('benefiters_legal_status')->where('benefiter_id', '=', $benefiterId)->get();
+            $benefiterLegalStatuses = $this->getBenefiterLegalStatusesFineArray($benefiterLegalStatuses);
+            $benefiterLanguages = \DB::table('benefiters_languages')->where('benefiter_id', '=', $benefiterId)->get();
+            $benefiterReferrals = \DB::table('benefiter_referrals')->where('benefiter_id', '=', $benefiterId)->get();
         }
     }
 
@@ -30,6 +52,35 @@ class DownloadFileService{
 
     // returns all the genders stored in the DB
     private function getAllGenders(){
-        return \DB::table('genders_lookup')->get()->toArray();
+        return \DB::table('genders_lookup')->get();
+    }
+
+    // returns all the referrals stored in DB
+    private function getAllReferrals(){
+        return \DB::table('benefiter_referrals_lookup')->get();
+    }
+
+    // get benefiter's legal statuses and transform the legal_status id to legal_status name
+    private function getBenefiterLegalStatusesFineArray($benefiterLegalStatuses){
+        if(!empty($benefiterLegalStatuses) and !empty($this->allLegalStatuses)) {
+            $result = array();
+            foreach ($benefiterLegalStatuses as $benefiterLegalStatus){
+                $tmp = array();
+                // try to find the legal status in DB result array
+                try {
+                    $legalStatusInfoFromLookup = $this->allLegalStatuses[$benefiterLegalStatus->legal_lookup_id];
+                } catch (\Exception $e){ // out of bound exception
+                    Log::error("Can't find the legal status requested.\n" . $e);
+                    continue;
+                }
+                $tmp['legal_status'] = $legalStatusInfoFromLookup->description;
+                $tmp['description'] = $benefiterLegalStatus->description;
+                $tmp['exp_date'] = $benefiterLegalStatus->exp_date;
+                array_push($result, $tmp);
+            }
+            return $result;
+        } else {
+            return null;
+        }
     }
 }
