5f20aa970dc70b0c4060b8900fef07ff617cc71c
spyninja <nikolakispyros@gmail.com>
2016-03-31 16:51:40 +0300

    BEN-385 fixed big margins and paddings in medical visit info printable modal view [top and bottom css problems still exist]

--- resources/views/partials/modals/medical_visit_info.blade.php
+++ resources/views/partials/modals/medical_visit_info.blade.php
@@ -371,7 +371,7 @@
 
                         </div>
                         {{-- supplied from PRAKSIS --}}
-                        <div class="col-xs-3">
+                        <div class="col-xs-3 @if(empty($med_medication['supply_from_praksis']) or $med_medication['supply_from_praksis'] == 0) non-printable @endif">
                             {!! Form::label('supply', Lang::get($p.'supply_from_praksis')) !!}
                             @if($med_medication['supply_from_praksis'] == 1)
                                 {!! Form::checkbox('supply', 1, true, array('class'=>'supply_from_praksis make-inline', 'disabled' => 'disabled')) !!}
